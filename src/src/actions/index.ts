ba7ecad8fad7c41303a7809384a1ecc6104edeae
johnsmith0951 <skybird1233@gmail.com>
2018-10-25 21:55:04 +1100

    Fixed conflicts

--- src/src/actions/index.ts
+++ src/src/actions/index.ts
@@ -1,12 +1,9 @@
 import { location } from '@hyperapp/router'
-<<<<<<< HEAD:src/src/actions/index.js
-=======
 import { apollo } from "hyperapp-apollo"
 
->>>>>>> staging:src/src/actions/index.ts
 export default {
   apollo: apollo.actions,
-  add: (/* event (e) */) => ({ num }) => ({ num: num + 1 }),
-  sub: (/* event (e) */) => ({ num }) => ({ num: num - 1 }),
+  add: (/* event (e) */) => ({ num: int }) => ({ num: num + 1 }),
+  sub: (/* event (e) */) => ({ num: int }) => ({ num: num - 1 }),
   location: location.actions
 };
