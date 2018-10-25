ecf607cce9294a48d6ce3fc09c85e5c6df061216
johnsmith0951 <skybird1233@gmail.com>
2018-10-25 21:35:46 +1100

    Remove unused modules

--- src/src/index.ts
+++ src/src/index.ts
@@ -1,9 +1,7 @@
-import { h, app } from 'hyperapp'
-import { Link, Route, location } from '@hyperapp/router'
+import { app } from 'hyperapp'
 
 import actions from './actions'
 import state from './state'
 import view from './App'
 
 const main = app(state, actions, view, document.body)
-const unsubscribe = location.subscribe(main.location)
