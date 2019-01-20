700990ba16b04e710b5c0a96eb304eb3da53d027
kai <skybrid1233@gmail.com>
2019-01-20 11:10:48 +1100

    refs 23 CeateCategoryのクエリを修正

--- src/montage-client/src/constants/category-query.ts
+++ src/montage-client/src/constants/category-query.ts
@@ -1,23 +1,12 @@
 import gql from 'graphql-tag';
 
-// export const CreateCategory = gql`
-//     mutation {
-//       createCategory(input: { name: "プログラミン2", description: "ピクミン2"})
-//       {
-//         category{
-//           name
-//           description
-//         }
-//       }
-//     }
-
 export const CreateCategory = gql`
-mutation ($input: dict) {
-  createCategory(input: $input){
-      category{
-        name
-        description
-      }
+  mutation ($input: CreateCategoryMutationInput!) {
+    createCategory(input: $input){
+        category{
+          name
+          description
+        }
     }
   }
-  `;
+`;
