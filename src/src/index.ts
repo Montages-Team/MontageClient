c68fdc51c7987284f482cad46b5ab6630f24109a
M.Kai <skybird1233@gmail.com>
2019-01-22 17:11:03 +1100

    ディレクトリを再構成 (#30)

--- src/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { app } from 'hyperapp'
-
-import actions from './actions'
-import state from './state'
-import view from './App'
-
-export const main = app(state, actions, view, document.body)
