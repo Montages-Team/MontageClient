93cc135fcad00ad2bfbaaefe3b7329cc11ff6878
raguna2 <kutsumi.for.public@gmail.com>
2019-09-07 21:56:35 +0900

    $authのtypescriptの型を独自定義

--- src/src/shims-vue.d.ts
+++ src/src/shims-vue.d.ts
@@ -1,4 +1,13 @@
+// このファイルはVueでtypescriptを使うときの独自の型定義です
+import Vue from 'vue';
+
 declare module '*.vue' {
-  import Vue from 'vue';
   export default Vue;
 }
+
+declare module 'vue/types/vue' {
+  interface Vue {
+    $auth: any;
+  }
+}
+
