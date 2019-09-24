4508169f5c6a14f8ff4972c98fb4bcbad5cd0b45
M.Kai <skybird1233@gmail.com>
2019-09-24 22:49:14 +0900

    ヘッダーユーザ登録/ログインボタンのデザイン (#88)
    * ヘッダにAuthButtonを追加
    
    * fix font-size and padding size
    
    * fix nits
    
    * rename AuthButton to SubButton

---
+++ src/src/components/atoms/SubButton.vue
@@ -0,0 +1,22 @@
+<template lang='pug'>
+    button {{ label }}
+</template>
+
+<script lang='ts'>
+import { Component, Vue, Prop } from 'vue-property-decorator';
+
+@Component({})
+export default class SubButton extends Vue {
+  @Prop({ type: String }) private label!: string;
+}
+</script>
+
+<style lang="stylus" scoped>
+button
+    color white
+    border white 1px solid
+    background none
+    font-size 90%
+    padding 7px 10px
+    border-radius 5px
+</style>
