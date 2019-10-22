b5e178b076fc4eb77c0d9883fb54879f381ecfa2
raguna2 <kutsumi.for.public@gmail.com>
2019-10-22 17:27:45 +0900

    [fix] Footerのデザインを修正

--- src/src/components/organisms/Footer.vue
+++ src/src/components/organisms/Footer.vue
@@ -1,14 +1,18 @@
 <template lang="pug">
   div
-    ul.footer-link-box
-      li
-        router-link.footer-margin(to="/terms/") 利用規約
-      li
-        router-link.footer-margin(to="/faq/") よくある質問
-      li
-        router-link.footer-margin(to="/privacy_policy/") プライバシーポリシー
-      li
-        a.footer-margin(href="") お問い合わせ
+    div.footer-link-box
+      div
+        router-link.link-style(to="/") ヘルプ
+        router-link.link-style(to="/") このサイトについて
+        router-link.link-style(to="/terms/") 利用規約
+        router-link.link-style(to="/privacy_policy/") プライバシーポリシー
+        router-link.link-style(to="/") Collageボタンについて
+        a.link-style(href="https://form.run/@montage") お問い合わせ
+        router-link.link-style(to="/") 運営について
+        a.link-style(href="https://twitter.com/montage_bio") 公式Twitter
+      router-link.cp-style(to="/")
+        i.creative.commons.icon
+        | 2019 Montage
 </template>
 
 <script lang='ts'>
@@ -21,47 +25,20 @@ export default class Footer extends Vue {
 
 <style lang="stylus" scoped>
 .footer-link-box
-  display: inline-block;
-  vertical-align: middle;
-  margin: 50px 0;
-  font-size: 0;
+  text-align center
+  padding 40px 20px
 
-.footer-link-box li
-  margin: 0 10px;
-  display: inline-block;
-  line-height: 1.8;
-  font-size: 14px;
-  letter-spacing: 0.05em;
-
-.gray-link
-  color #888888 !important
-
-.image-margin
-  margin 0 5px 0 12px !important
-
-.social-round-icon
-  width auto !important
-
-.footer-margin
-  margin 8px !important
-
-li
-  margin 0 10px
+.link-style
   display inline-block
-  line-height 1.8
+  color #9e9e9e
+  font-size 12px
+  padding 5px
+  text-decoration none
+
+.cp-style
+  display block
+  color #AAA
   font-size 14px
-  letter-spacing 0.05em
-
-@media screen and (max-width: 1100px)
-  .footer-link-box
-    margin 0px 12%
-
-@media screen and (max-width: 900px)
-  // タブレットより小さいものはこっちが適用
-  .footer-link-box
-    margin 30px 0 20px 0
-
-  .footer li
-    font-size 13px
-
+  padding 5px
+  text-decoration none
 </style>
