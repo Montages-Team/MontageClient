c72b915293e82abe325300769620bca697e40acb
raguna2 <kutsumi.for.public@gmail.com>
2019-12-01 11:28:42 +0900

    [fix] ログインボタンの近くに利用規約とプライバシーポリシーへのリンクを配置

--- src/src/components/pages/Home.vue
+++ src/src/components/pages/Home.vue
@@ -4,6 +4,10 @@
       img.logo(src='@/assets/icon.svg')
       h5.catch_copy(is='sui-header') 友達も、好きな人も、自由にイジろう
       DimButton(label='はじめる')
+      div.term-policy-block
+        router-link(to="/terms/") 利用規約
+        span /
+        router-link(to="/privacy_policy") プライバシーポリシー
       div
         i.angle.double.down.icon(style="color: #ffffff;")
       h5.catch_copy(is='sui-header') Montageとは?
@@ -49,6 +53,18 @@ export default class Home extends Vue {}
   margin 4px 0px !important
   color #FFFFFF !important
   font-weight unset !important
+
+.term-policy-block
+  text-align center
+  font-size 13px
+  color #999 !important
+  padding 12px
+
+.term-policy-block > a, .term-policy-block > span
+  display inline-block
+  color #ffffff
+  margin 0px 3px
+  font-weight bold
 
 @media screen and (min-width:1100px)
   .flex-column
