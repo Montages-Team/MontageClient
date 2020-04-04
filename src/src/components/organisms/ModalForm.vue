71e619f6f9b9f8c8166a16b09ae21cc0fb18a37b
raguna2 <kutsumi.for.public@gmail.com>
2020-04-04 12:13:07 +0900

    [fix] 投稿フォームの文字制限を表示＆背景固定

--- src/src/components/organisms/ModalForm.vue
+++ src/src/components/organisms/ModalForm.vue
@@ -4,11 +4,18 @@
       .modal-mask(v-show="open")
         .modal-wrapper
           .modal-container
+            .modal-header(v-show="isInvalidInput" style="width: inherit;")
+              sui-message(warning style="margin: 4px; width: inherit;") 文字数が不正です.
             .modal-header
               ConfirmButton.button-cancel(content="キャンセル" @cancelImpression="toggleOpen")
               ConfirmButton(:content='postButtonContent' @emitPost="postMutation")
             .modal-body
-              textarea.modalform__body.font-size__small(v-model="impressionInput" :placeholder="placeholder")
+              textarea.modalform__body.font-size__small(
+                v-model="impressionInput"
+                :placeholder="placeholder"
+                :maxlength="maxCount")
+              sui-container(text-align='right')
+                p(style="color: #AAAAAA;") {{ this.impressionInput.length }}/42
     sui-modal.dimmed(v-else v-model="open" size="tiny")
       sui-modal-header
         span(@click="toggleOpen")
@@ -17,9 +24,14 @@
         .modal-body
           sui-form(inverted)
             sui-form-field
-              input.modal-fontsize(v-model="impressionInput" :placeholder="placeholder")
-              p {{ this.impressionInput.length }}/42
-              p {{ this.impressionInput }}
+              input.modal-fontsize(
+                v-model="impressionInput"
+                :placeholder="placeholder"
+                :maxlength="maxCount")
+              sui-container(text-align='right')
+                p(style="color: #AAAAAA;") {{ this.impressionInput.length }}/42
+      .modal-header(v-show="isInvalidInput" style="width: inherit;")
+        sui-message(warning style="margin: 4px; width: inherit;") 文字数が不正です.
       .modal-header
         ConfirmButton(:content='postButtonContent' @emitPost="postMutation")
 </template>
@@ -43,6 +55,8 @@ const ConfirmButton = () => import(
 export default class ModalForm extends Vue {
   private impressionInput: string = '';
   private open: boolean = false;
+  private maxCount: number = 42;
+  private isInvalidInput: boolean = false;
 
   @Prop({ type: Number })
   private selectedQuestionId!: number;
@@ -66,6 +80,9 @@ export default class ModalForm extends Vue {
   @Emit()
   public toggleOpen(): void {
     this.open = !this.open;
+    if (this.open === false) {
+      this.isInvalidInput = false;
+    }
   }
 
   private get isMobile() {
@@ -74,38 +91,46 @@ export default class ModalForm extends Vue {
 
   @Emit()
   private postMutation(isCollage: boolean) {
-    const mutation = this.$apollo.mutate({
-      mutation: CreateImpressionMutation,
-      variables: {
-        content: this.impressionInput,
-        userName:  this.$route.params.userName,
-        questionId: this.selectedQuestionId,
-      },
-    });
-
-    mutation
-      .then(({ data }) => {
-        if (!data) {
-          throw new Error('data is undefined');
-        }
-        const res = data.createImpression;
-        return res;
-      })
-      .then(({ ok, impression, errors }) => {
-        if (ok) {
-          console.log(this.$route.name);
-          if (this.$route.name === 'profile') {
-            this.$emit('toggle');
-            this.impressionInput = '';
-          } else if (this.$route.name === 'individual') {
-            this.$router.push(`/profile/${this.$route.params.userName}`);
-          } else {
-            this.$router.push(`/profile/${this.$route.params.userName}`);
-          }
-        }
-      }).catch((error) => {
-        console.log(error);
+    if (this.impressionInput.length > 0 && this.impressionInput.length < 43) {
+      this.isInvalidInput = false;
+      const mutation = this.$apollo.mutate({
+        mutation: CreateImpressionMutation,
+        variables: {
+          content: this.impressionInput,
+          userName:  this.$route.params.userName,
+          questionId: this.selectedQuestionId,
+        },
       });
+      mutation
+        .then(({ data }) => {
+          if (!data) {
+            throw new Error('data is undefined');
+          }
+          const res = data.createImpression;
+          return res;
+        })
+        .then(({ ok, impression, errors }) => {
+          if (ok) {
+            console.log(this.$route.name);
+            if (this.$route.name === 'profile') {
+              this.$emit('toggle');
+              this.impressionInput = '';
+            } else if (this.$route.name === 'individual') {
+              this.$router.push(`/profile/${this.$route.params.userName}`);
+            } else {
+              this.$router.push(`/profile/${this.$route.params.userName}`);
+            }
+            const modal: any = document.querySelector('.modals');
+            enableBodyScroll(modal);
+          }
+        }).catch((error) => {
+          console.log(error);
+        });
+    } else {
+      console.error('invalid input length.');
+      this.isInvalidInput = true;
+      return;
+    }
   }
 }
 </script>
