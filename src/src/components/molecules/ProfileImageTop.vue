3cf41295e4853363c06ca6af72642990dc9903f6
raguna2 <kutsumi.for.public@gmail.com>
2020-04-23 02:04:39 +0900

    [fix] スマホ版のユーザトップ画像を変更

--- src/src/components/molecules/ProfileImageTop.vue
+++ src/src/components/molecules/ProfileImageTop.vue
@@ -1,11 +1,25 @@
 <template lang='pug'>
   div
-    div.overlay-area(v-if="isMobile")
-      sui-image.picture-area(:src="user.profileImgUrl" fluid alt='プロフィール画像')
-      div.float-area
-        div(style="width: 50%;")
-          div.displayname {{user.displayName}}
-          div.username.font-size__small @{{user.username}}
+    div.overlay-area(v-if="isMobile" :style="{ backgroundImage: 'url(' + 'https://cdn.pixabay.com/photo/2015/03/26/09/59/purple-690724_960_720.jpg' + ')' }")
+      sui-dropdown.icon.montage-dropdown(icon='ellipsis horizontal' button pointing)
+        sui-dropdown-menu
+          sui-dropdown-item
+            router-link(:to="{ name: 'settings'}")
+              sui-icon(name='wrench')
+              | 設定
+          sui-dropdown-item
+            a(:href='twitterLink')
+              sui-icon(name='twitter')
+              | Twitter
+          //- sui-dropdown-item
+          //-   sui-icon(name='images')
+          //-   | 写真の変更
+      sui-statistic(style="width: 100vw; padding: 40px;")
+        sui-statistic-value
+          img.ui.circular.inline.image(:src='user.profileImgUrl' size="small")
+        sui-statistic-label(style="font-size: 18px; color: #fff")
+          div(style="font-size: 16px; color: #d9d9d9") {{user.displayName}}
+          div(style="font-size: 12px; color: #d9d9d9") @{{user.username}}
     div.overlay-area(v-else)
       sui-card
         sui-card-content.card-content-style-pc
@@ -97,4 +111,10 @@ export default class ProfileImageTop extends Vue {
   margin 0px 14% !important
   width 72% !important
   color #fff !important
+
+.montage-dropdown
+  color #fff !important
+  margin 4px 0px 0px 8px !important
+  background none !important
+  font-size 24px !important
 </style>
