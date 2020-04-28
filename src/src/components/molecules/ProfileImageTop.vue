5cbf049e7a8a34c6084d7a44b3784d09d93cc2ba
raguna2 <kutsumi.for.public@gmail.com>
2020-04-28 20:22:56 +0900

    [fix] 認証ユーザ以外のページでは設定へ遷移できないようにした

--- src/src/components/molecules/ProfileImageTop.vue
+++ src/src/components/molecules/ProfileImageTop.vue
@@ -3,7 +3,7 @@
     div.overlay-area(v-if="isMobile" :style="{ backgroundImage: 'url(' + 'https://cdn.pixabay.com/photo/2015/03/26/09/59/purple-690724_960_720.jpg' + ')' }")
       sui-dropdown.icon.montage-dropdown(icon='ellipsis horizontal' button pointing)
         sui-dropdown-menu
-          sui-dropdown-item
+          sui-dropdown-item(v-if="this.$parent.displayName === this.$route.params.userName")
             router-link(:to="{ name: 'settings'}")
               sui-icon(name='wrench')
               | 設定
